3f3a3587b83a6514205048d6edb588d7216b511d
Slava Richkov <v.v.richkov@yandex.ru>
2022-12-26 15:43:12 +0500

    feat: инициализация приложения

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import './style/app.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="header">
+        <h1>Hi</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
