8334d87ff342e885178eba4e263212eb25626e8a
Slava Richkov <v.v.richkov@yandex.ru>
2022-12-27 14:02:07 +0500

    feat: компонент Info

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,11 +2,13 @@ import './style/app.css';
 import React from 'react';
 
 import Header from './../Header/Header' // импорт Header
+import Info from './../Info/Info' // импорт Info 
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Info />
     </div>
   );
 }
---
+++ src/components/Info/Info.js
@@ -0,0 +1,14 @@
+function Info() {
+
+    return (
+        <div className='info'>
+            <p className='info_text'>Уважаемые участники, во время вашего хода вы можете изменить параметры торгов, указанные в таблице:</p>
+            <div className='info_motion'>
+                <p className='info_motion-text'>ХОД</p>
+                <div className='info_timer'></div>
+            </div>
+        </div>
+    );
+}
+
+export default Info;
