63636348c3e6c079f79b17599a92163ca6affe8e
Slava Richkov <v.v.richkov@yandex.ru>
2022-12-27 10:00:07 +0500

    feat: date and time

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,15 @@
 import './style/app.css';
+import React from 'react';
+
+import DateAndTime from '../../utils/DateAndTime/DateAndTime' // импорт даты и времени
 
 function App() {
+
   return (
     <div className="app">
       <header className="header">
-        <h1>Hi</h1>
+        <h2 className='header_title'>Ход торгов</h2>
+        <p className='header_text'>Тестовые торги на аппарат ЛОТОС №2033564 ({DateAndTime()}) </p>
       </header>
     </div>
   );
---
+++ src/utils/DateAndTime/DateAndTime.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+function DateAndTime() {
+    //дата и время
+    const [isDate, setIsDate] = useState(new Date());
+    setInterval(() => setIsDate(new Date()), 1000); // обновить дату раз в секунду
+
+    //дата
+    let date = () => { if (isDate.getDate() < 10) { return '0' + isDate.getDate() } else { return isDate.getDate() } }; // число
+    let month = () => { if (isDate.getMonth() < 10) { return '0' + isDate.getMonth() } else { return isDate.getMonth() } }; // месяц
+    let dateNow = date() + '.' + month() + '.' + isDate.getFullYear(); // получить дату;
+
+    // время
+    let hours = () => { if (isDate.getHours() < 10) { return '0' + isDate.getHours() } else { return isDate.getHours() } }; // часы
+    let minutes = () => { if (isDate.getMinutes() < 10) { return '0' + isDate.getMinutes() } else { return isDate.getMinutes() } }; // минуты
+    let seconds = () => { if (isDate.getSeconds() < 10) { return '0' + isDate.getSeconds() } else { return isDate.getSeconds() } }; // секунды
+    let timeNow = hours() + ':' + minutes() + ':' + seconds(); // получить время
+
+    return dateNow + ' ' + timeNow;
+
+};
+
+export default DateAndTime;
