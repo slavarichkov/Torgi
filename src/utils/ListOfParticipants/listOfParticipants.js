153613f1b50ba6e55e34cc200d71bf390cb841f6
Slava Richkov <v.v.richkov@yandex.ru>
2023-01-03 18:16:08 +0500

    feat: список участников(фикс)

---
+++ src/utils/ListOfParticipants/listOfParticipants.js
@@ -0,0 +1,28 @@
+const listOfParticipants = [
+    {
+        name: 'ООО Фирма 1',
+        productionTime: '80',
+        warranty: '24',
+        paymentTerms: '30',
+        cost: '100',
+        qualityStandards: '',
+    },
+    {
+        name: 'ООО Фирма 2',
+        productionTime: '80',
+        warranty: '24',
+        paymentTerms: '30',
+        cost: '100',
+        qualityStandards: '',
+    },
+    {
+        name: 'ООО Фирма 3',
+        productionTime: '80',
+        warranty: '24',
+        paymentTerms: '30',
+        cost: '100',
+        qualityStandards: '',
+    },
+]
+
+export default listOfParticipants;
