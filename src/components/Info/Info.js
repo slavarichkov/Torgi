651b5f2c2b9d92f73a5c0fec8efdcddca8c63608
Slava Richkov <v.v.richkov@yandex.ru>
2023-01-04 14:35:55 +0500

    refactoring: перенос в компонент Tenderer

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import Timer from './../../utils/Timer/Timer';
-import Timer1 from '../../utils/Timer/Timer1';
 
 function Info() {
 
@@ -10,8 +8,6 @@ function Info() {
         <div className='info'>
             <p className='info_text'>Уважаемые участники, во время вашего хода вы можете изменить параметры торгов, указанные в таблице:</p>
             <div className='info_motion'>
-                {/* <Timer1 /> */}
-                {/* <Timer /> */}
             </div>
         </div>
     );
